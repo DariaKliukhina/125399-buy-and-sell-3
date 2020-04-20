657173911d79add82839a6cf1f007fa3e2dfa29d
Daria Kliukhina <d.kliukhina@pink-man.ru>
2020-04-20 23:47:03 +0300

    Выносит контент в текстовые файлы

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -6,39 +6,12 @@ const {
   printNumWithLead0
 } = require(`../../utils`);
 const fs = require(`fs`).promises;
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
-
-const TITLES = [
-  `Продам книги Стивена Кинга`,
-  `Продам новую приставку Sony Playstation 5`,
-  `Продам отличную подборку фильмов на VHS`,
-  `Куплю антиквариат`,
-  `Куплю породистого кота`,
-];
-
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-];
-
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`,
-];
 
 const OfferType = {
   offer: `offer`,
@@ -56,23 +29,33 @@ const SumRestrict = {
   max: 100000,
 };
 
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
+
 const getPictureFileName = (number) => `item${printNumWithLead0(number)}.jpg`;
 const generatePicture = () => getPictureFileName(getRandomInt(PictureRestrict.min, PictureRestrict.max));
 
-const generateCategory = () => CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)];
-const generateDescription = () => shuffle(SENTENCES).slice(1, 5).join(` `);
-const generateTitle = () => TITLES[getRandomInt(0, TITLES.length - 1)];
+const generateCategory = (categories) => categories[getRandomInt(0, categories.length - 1)];
+const generateDescription = (sentences) => shuffle(sentences).slice(1, 5).join(` `);
+const generateTitle = (titles) => titles[getRandomInt(0, titles.length - 1)];
 const generateType = () => Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)];
 
-const generateOffers = (count) => {
+const generateOffers = (count, titles, categories, sentences) => {
   let offersArray = [];
 
   for (let i = 0; i < count; i++) {
     offersArray.push({
-      category: [generateCategory()],
-      description: generateDescription(),
+      category: [generateCategory(categories)],
+      description: generateDescription(sentences),
       picture: generatePicture(),
-      title: generateTitle(),
+      title: generateTitle(titles),
       type: generateType(),
       sum: getRandomInt(SumRestrict.min, SumRestrict.max),
     });
@@ -84,9 +67,13 @@ const generateOffers = (count) => {
 module.exports = {
   name: `--generate`,
   async run(args) {
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const offers = generateOffers(countOffer);
+    const offers = generateOffers(countOffer, titles, categories, sentences);
     const content = JSON.stringify(offers);
 
     try {
