675a77277ea004305ee20ae595c0eadef00644bc
Daria Kliukhina <d.kliukhina@pink-man.ru>
2020-04-26 18:13:55 +0300

    Переделать чтение файлов на promise.all

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -29,13 +29,13 @@ const SumRestrict = {
   max: 100000,
 };
 
-const readContent = async (filePath) => {
+const readContent = async (filePath, object, key) => {
   try {
     const content = await fs.readFile(filePath, `utf8`);
-    return content.split(`\n`);
+    object[key] = content.split(`\n`);
+    return object;
   } catch (err) {
-    console.error(chalk.red(err));
-    return [];
+    throw err;
   }
 };
 
@@ -66,21 +66,25 @@ const generateOffers = (count, titles, categories, sentences) => {
 
 module.exports = {
   name: `--generate`,
-  async run(args) {
-    const sentences = await readContent(FILE_SENTENCES_PATH);
-    const titles = await readContent(FILE_TITLES_PATH);
-    const categories = await readContent(FILE_CATEGORIES_PATH);
+  run(args) {
 
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const offers = generateOffers(countOffer, titles, categories, sentences);
-    const content = JSON.stringify(offers);
 
-    try {
-      await fs.writeFile(FILE_NAME, content);
-      console.log(chalk.green(`Operation success. File created.`));
-    } catch (err) {
-      console.error(chalk.red(`Can't write data to file...`));
-    }
+    const data = {};
+    Promise
+      .all([
+        readContent(FILE_SENTENCES_PATH, data, `sentences`),
+        readContent(FILE_TITLES_PATH, data, `titles`),
+        readContent(FILE_CATEGORIES_PATH, data, `categories`)
+      ]).then(() => {
+        const offers = generateOffers(countOffer, data.titles, data.categories, data.sentences);
+        const content = JSON.stringify(offers);
+        fs.writeFile(FILE_NAME, content);
+      }).then(() => {
+        console.log(chalk.green(`Operation success. File created.`));
+      }).catch((err) => {
+        console.error(chalk.red(`Can't write data to file with error: ${err}`));
+      });
   }
 };
