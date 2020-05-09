98a4ae9e58bf1b709cd161d0a3f30208f72907d1
Daria Kliukhina <d.kliukhina@pink-man.ru>
2020-05-09 17:15:54 +0300

    Удалить дублированную строку

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -7,6 +7,5 @@ offersRouter.get(`/category/:id`, (req, res) => res.send(`/offers/category/:id`)
 offersRouter.get(`/add`, (req, res) => res.send(`/offers/add`));
 offersRouter.get(`/edit/:id`, (req, res) => res.send(`/offers/edit/:id`));
 offersRouter.get(`/:id`, (req, res) => res.send(`/offers/:id`));
-offersRouter.get(`/:id`, (req, res) => res.send(`/offers/:id`));
 
 module.exports = offersRouter;
